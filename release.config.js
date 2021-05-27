9bcd21bfc4c29e2d3ac385a128ac64f06ca8ebce
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-27 15:26:53 +0530

    ci: added semantic git plugin to update package.json

--- release.config.js
+++ release.config.js
@@ -6,5 +6,13 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
