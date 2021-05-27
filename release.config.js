43243366f0d5beed3afe2b55f665793051b67de6
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-27 12:34:19 +0530

    ci: added github action to publish npm package

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: 'main',
+  repositoryUrl:
+    'https://github.com/karthickthankyou/bulletproof-boilerplate-train',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+  ],
+};
