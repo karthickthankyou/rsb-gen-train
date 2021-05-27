c475efe0119ce3277a8661ed562c164c5a08c34a
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-27 12:41:30 +0530

    fix(scripts): fixed format

--- templates.js
+++ templates.js
@@ -1,9 +1,9 @@
 // component.tsx
 exports.component = (
   componentName,
-) => `export interface I${componentName}Props {}
+) => `export interface I${componentName}Props { }
 
-  const ${componentName} = ({}: I${componentName}Props) => {
+  const ${componentName} = ({ }: I${componentName}Props) => {
     return (
       <div>
         Hello, This is ${componentName} component.
@@ -16,32 +16,32 @@ exports.component = (
 
 // component.stories.tsx
 exports.story = (componentName, componentType) => `import React from 'react';
-  import { Story, Meta } from '@storybook/react/types-6-0';
-  import ${componentName}, {I${componentName}Props} from './${componentName}';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import ${componentName}, { I${componentName}Props} from './${componentName}';
 
-  export default {
+export default {
     title: '${componentType || 'Pages'}/${componentName}',
     component: ${componentName},
-  } as Meta;
+} as Meta;
 
-  const Template: Story<I${componentName}Props> = (args) => <${componentName} {...args} />;
+const Template: Story<I${componentName}Props> = (args) => <${componentName} {...args} />;
 
-  export const Primary = Template.bind({});
-  Primary.args = {};
-  `;
+export const Primary = Template.bind({});
+Primary.args = {};
+`;
 
 // component.test.tsx
 exports.testScript = (componentName) => `import React from 'react';
-  import { render } from '@testing-library/react';
-  import ${componentName} from './${componentName}';
+import { render } from '@testing-library/react';
+import ${componentName} from './${componentName}';
 
-  describe('${componentName} Component', () => {
-    test('it should match the snapshot', () => {
-      const { asFragment } = render(<${componentName} />);
-      expect(asFragment()).toMatchSnapshot();
-    });
+describe('${componentName} Component', () => {
+  test('it should match the snapshot', () => {
+    const { asFragment } = render(<${componentName} />);
+    expect(asFragment()).toMatchSnapshot();
   });
-  `;
+});
+`;
 
 // index.ts
 exports.barrel = (
