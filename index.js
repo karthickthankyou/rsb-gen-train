8b365a097132168a904e89c3c7a86c2c15ba4827
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-10-07 20:05:09 +0530

    fix: added testing script generation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const chalk = require('chalk');
-const fs = require('fs');
-let { component, story, testScript, barrel } = require('./templates.js');
+const chalk = require('chalk')
+const fs = require('fs')
+let { component, story, testScript, barrel } = require('./templates.js')
 
 // grab component componentName from terminal argument
-const [componentName] = process.argv.slice(2);
+const [componentName] = process.argv.slice(2)
 if (!componentName) {
-  console.clear();
-  console.log(chalk.yellow(`You must include a component name!`));
+  console.clear()
+  console.log(chalk.yellow(`You must include a component name!`))
   console.log(
     chalk.hex('#aaa')(`Example: `),
     chalk.white(
@@ -15,33 +15,33 @@ if (!componentName) {
         `(ex: components/cards)(optional)`,
       )}`,
     ),
-  );
-  console.log();
+  )
+  console.log()
   console.log(
     chalk.hex('#aaa')(
       `Component will be created under ${chalk.white(
         `src/`,
       )} if no path is specified.\n`,
     ),
-  );
+  )
 
-  process.exit(0);
+  process.exit(0)
 }
 
 // grab component type from terminal argument
-const [filePath] = process.argv.slice(3); // throw an error if the file path does not exit.
+const [filePath] = process.argv.slice(3) // throw an error if the file path does not exit.
 if (filePath && !fs.existsSync(`./src/${filePath}`)) {
   console.log(
     chalk.red(`
     Error! Component path ${`./src/${filePath}`} does not exist.
   `),
-  );
-  process.exit(0);
+  )
+  process.exit(0)
 }
 
 const dir = filePath
   ? `./src/${filePath}/${componentName}`
-  : `./src/${componentName}`;
+  : `./src/${componentName}`
 
 // throw an error if the file already exists
 if (fs.existsSync(dir)) {
@@ -49,15 +49,15 @@ if (fs.existsSync(dir)) {
     chalk.red(`
     Component ${dir} already exists.
   `),
-  );
-  process.exit(0);
+  )
+  process.exit(0)
 }
 
 // create the folder
-fs.mkdirSync(dir);
+fs.mkdirSync(dir)
 
 function writeFileErrorHandler(err) {
-  if (err) throw err;
+  if (err) throw err
 }
 
 // component.tsx
@@ -65,26 +65,26 @@ fs.writeFile(
   `${dir}/${componentName}.tsx`,
   component(componentName),
   writeFileErrorHandler,
-);
+)
 // storybook.jsx
 fs.writeFile(
   `${dir}/${componentName}.stories.tsx`,
   story(componentName, filePath),
   writeFileErrorHandler,
-);
+)
 // test.tsx
 // Test will be added again with better arguments.
-// fs.writeFile(
-//   `${dir}/${componentName}.test.tsx`,
-//   testScript(componentName),
-//   writeFileErrorHandler
-// );
+fs.writeFile(
+  `${dir}/${componentName}.test.tsx`,
+  testScript(componentName),
+  writeFileErrorHandler,
+)
 // index.tsx
-fs.writeFile(`${dir}/index.ts`, barrel(componentName), writeFileErrorHandler);
+fs.writeFile(`${dir}/index.ts`, barrel(componentName), writeFileErrorHandler)
 
 console.log(
   chalk.green(`
   Success! The component ${componentName} is created.
   Path: ${dir}
   `),
-);
+)
